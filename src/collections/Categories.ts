2402e802be66508f28fc723ca88a1951123bc1a1
Shekhar Anand Chaudhary <shekharchaudhary870950@gmail.com>
2025-08-22 14:08:55 +0530

    Payload Integration

---
+++ src/collections/Categories.ts
@@ -0,0 +1,17 @@
+import type { CollectionConfig } from "payload";
+
+export const Categories: CollectionConfig = {
+  slug: "categories",
+  access: {
+    create: () => false,
+    update: () => false,
+  },
+  fields: [
+    {
+      name: "name",
+      type: "text",
+      required: true,
+    },
+  ],
+  upload: true,
+};
