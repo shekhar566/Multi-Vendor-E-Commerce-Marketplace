4f93399e346295a874745fffaf76a28f6559abd6
Shekhar Anand Chaudhary <shekharchaudhary870950@gmail.com>
2025-10-14 00:02:47 +0530

    12: filtering products fix 2

--- src/modules/products/server/procedures.tsx
+++ src/modules/products/server/procedures.tsx
@@ -15,17 +15,18 @@ export const productsRouter = createTRPCRouter({
     .query(async ({ ctx, input }) => {
       const where: Where = {};
 
-      if (input.minPrice) {
+      if (input.minPrice && input.maxPrice) {
         where.price = {
-          ...where.price,
-          less_than_equal: input.minPrice,
+          greater_than_equal: input.minPrice,
+          less_than_equal: input.maxPrice,
         };
-      }
-
-      if (input.maxPrice) {
+      } else if (input.minPrice) {
         where.price = {
-          ...where.price,
-          greater_than_equal: input.maxPrice,
+          greater_than_equal: input.minPrice,
+        };
+      } else if (input.maxPrice) {
+        where.price = {
+          less_than_equal: input.maxPrice,
         };
       }
       if (input.category) {
