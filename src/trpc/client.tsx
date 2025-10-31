2e017cef5e291909fcb443f6a6c8cecab4d08399
Shekhar Anand Chaudhary <shekharchaudhary870950@gmail.com>
2025-10-31 13:13:18 +0530

    17: product page

--- src/trpc/client.tsx
+++ src/trpc/client.tsx
@@ -22,13 +22,25 @@ function getQueryClient() {
   if (!browserQueryClient) browserQueryClient = makeQueryClient();
   return browserQueryClient;
 }
+// function getUrl() {
+//   const base = (() => {
+//     if (typeof window !== "undefined") return "";
+//     return process.env.NEXT_PUBLIC_APP_URL;
+//   })();
+//   return `${base}/api/trpc`;
+// }
 function getUrl() {
   const base = (() => {
     if (typeof window !== "undefined") return "";
+    console.log("🌍 Server base URL:", process.env.NEXT_PUBLIC_APP_URL);
     return process.env.NEXT_PUBLIC_APP_URL;
   })();
-  return `${base}/api/trpc`;
+
+  const fullUrl = `${base}/api/trpc`;
+  console.log("🚀 Final TRPC URL:", fullUrl);
+  return fullUrl;
 }
+
 export function TRPCReactProvider(
   props: Readonly<{
     children: React.ReactNode;
