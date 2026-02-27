62cf71a12df34bda673d55dc80012da23408a32f
Shekhar Anand Chaudhary <shekharchaudhary870950@gmail.com>
2026-02-27 13:02:13 +0530

    29: storage adapter

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,23 +1,25 @@
 // storage-adapter-import-placeholder
+import path from "path";
+import { fileURLToPath } from "url";
+
 import { mongooseAdapter } from "@payloadcms/db-mongodb";
 import { payloadCloudPlugin } from "@payloadcms/payload-cloud";
 import { multiTenantPlugin } from "@payloadcms/plugin-multi-tenant";
 import { lexicalEditor } from "@payloadcms/richtext-lexical";
-import path from "path";
+import { vercelBlobStorage } from "@payloadcms/storage-vercel-blob";
 import { buildConfig } from "payload";
-import { fileURLToPath } from "url";
 import sharp from "sharp";
 
-import { Users } from "./collections/Users";
-import { Media } from "./collections/Media";
 import { Categories } from "./collections/Categories";
+import { Media } from "./collections/Media";
+import { Orders } from "./collections/Orders";
 import { Products } from "./collections/Products";
+import { Reviews } from "./collections/Reviews";
 import { Tags } from "./collections/Tags";
 import { Tenants } from "./collections/Tenants";
-import { Config } from "./payload-types";
-import { Orders } from "./collections/Orders";
-import { Reviews } from "./collections/Reviews";
+import { Users } from "./collections/Users";
 import { isSuperAdmin } from "./lib/access";
+import { Config } from "./payload-types";
 
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
@@ -63,5 +65,12 @@ export default buildConfig({
       userHasAccessToAllTenants: (user) => isSuperAdmin(user),
     }),
     // storage-adapter-placeholder
+    vercelBlobStorage({
+      enabled: true,
+      collections: {
+        media: true,
+      },
+      token: process.env.BLOB_READ_WRITE_TOKEN,
+    }),
   ],
 });
